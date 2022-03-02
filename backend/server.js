c7c659f009d65565b006cc1347898038a8c1e499
emirsahovic <emir.sahovic00@gmail.com>
2022-03-02 15:39:25 +0100

    Initial login and registration endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from 'dotenv';
 import colors from 'colors';
 
+import userRoutes from './routes/userRoutes.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -11,5 +13,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Welcome to the Task Management API');
 })
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => console.log(`Server running on ${PORT} port`.yellow.bold));
