7ed486a3dd1f1b433aeb11ab48d5774d5b191da5
emirsahovic <emir.sahovic00@gmail.com>
2022-03-02 15:57:04 +0100

    Database connection

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,18 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import userRoutes from './routes/userRoutes.js';
 import { errorHandler } from "./middleware/errorMiddleware.js";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Task Management API');
