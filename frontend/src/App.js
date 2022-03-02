8f715e12c2e33c07017aac56290b2e7996fbeefa
emirsahovic <emir.sahovic00@gmail.com>
2022-03-02 21:23:45 +0100

    App routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components.jsx/Header";
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
