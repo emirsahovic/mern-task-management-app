7af7e1c105272bd4bba1894f5ed67e6c9263c8ca
emirsahovic <emir.sahovic00@gmail.com>
2022-03-02 16:04:27 +0100

    Registration and Login endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,73 @@
-const registerUser = (req, res) => {
-    res.status(200).json({ message: 'Register user' });
-}
+import expressAsyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
-const loginUser = (req, res) => {
-    res.status(200).json({ message: 'Login user' });
-}
+const registerUser = expressAsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please provide name, email and password');
+    }
+
+    if (password.length < 6) {
+        res.status(400);
+        throw new Error('Password must be at least 6 characters long');
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassowrd = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassowrd
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+})
+
+const loginUser = expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Please provide email and password');
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+})
 
 export { registerUser, loginUser }
