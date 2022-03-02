32ed8b653bb7ce23e8705067a76aea6029e55f51
emirsahovic <emir.sahovic00@gmail.com>
2022-03-02 15:45:09 +0100

    User model created

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        maxlength: 200
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6
+    },
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
