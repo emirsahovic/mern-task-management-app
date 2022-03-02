b221e4bade7ebfec08240c70d6f35a4ebf2ceb82
emirsahovic <emir.sahovic00@gmail.com>
2022-03-02 21:21:21 +0100

    Email validation moved to frontend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -9,11 +9,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Please add an email'],
-        unique: true,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            'Please add a valid email'
-        ]
+        unique: true
     },
     password: {
         type: String,
