9cb1a370b39bf64ec1536ea3c8c70a88027e709a
emirsahovic <emir.sahovic00@gmail.com>
2022-03-03 14:04:30 +0100

    Route for users task list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Register from './pages/Register';
 import Login from './pages/Login';
 import TaskForm from './pages/TaskForm';
+import MyTasks from './pages/MyTasks';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/tasks' element={<TaskForm />} />
+          <Route path='/my-tasks' element={<MyTasks />} />
         </Routes>
       </Router>
       <ToastContainer />
