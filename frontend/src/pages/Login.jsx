a4e282d091a95850ed0381bdc893e7c99c693b10
emirsahovic <emir.sahovic00@gmail.com>
2022-03-03 11:44:36 +0100

    Change the position of the form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -59,8 +59,8 @@ const Login = () => {
 
     return (
         <>
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                <div className="px-8 py-6 mx-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
+            <div className="flex justify-center min-h-screen bg-gray-100">
+                <div className="px-8 py-6 mx-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3 h-3/4 mt-28">
                     <div className="flex justify-center">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-20 text-blue-600" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
