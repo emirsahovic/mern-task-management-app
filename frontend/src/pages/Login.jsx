4e67118a8b31ad4f5e7a060586fefbda406596fd
emirsahovic <emir.sahovic00@gmail.com>
2022-03-04 11:56:04 +0100

    UI color modification

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
             <div className="flex justify-center min-h-screen bg-gray-100">
                 <div className="px-8 py-6 mx-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3 h-3/4 mt-28">
                     <div className="flex justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-20 text-blue-600" fill="none" viewBox="0 0 24 24"
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-20 text-teal-500" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path d="M12 14l9-5-9-5-9 5 9 5z" />
                             <path
@@ -77,7 +77,7 @@ const Login = () => {
                             <div className="mt-4">
                                 <label className="block" htmlFor="email">Email</label>
                                 <input type="email" placeholder="Email"
-                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
                                     name="email"
                                     value={email}
                                     onChange={onChange}
@@ -87,7 +87,7 @@ const Login = () => {
                             <div className="mt-4">
                                 <label className="block">Password</label>
                                 <input type="password" placeholder="Password"
-                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
                                     name="password"
                                     value={password}
                                     onChange={onChange}
@@ -95,13 +95,13 @@ const Login = () => {
                                 />
                             </div>
                             <div className="flex">
-                                <button type='submit' className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 transition duration-200">
+                                <button type='submit' className="w-full px-6 py-2 mt-4 text-white bg-teal-500 rounded-lg hover:bg-teal-700 transition duration-200">
                                     Log In
                                 </button>
                             </div>
                             <div className="mt-6 text-grey-dark">
                                 <span className="mr-2">Don't have an account?</span>
-                                <Link to='/' className="text-blue-600 hover:underline font-bold">
+                                <Link to='/' className="text-teal-600 hover:underline font-bold">
                                     Register
                                 </Link>
                             </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -67,7 +67,7 @@ const Register = () => {
             <div className="flex justify-center min-h-screen bg-gray-100">
                 <div className="px-8 py-6 mx-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3 h-3/4 mt-28">
                     <div className="flex justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-20 text-blue-600" fill="none" viewBox="0 0 24 24"
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-20 text-teal-600" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path d="M12 14l9-5-9-5-9 5 9 5z" />
                             <path
@@ -82,7 +82,7 @@ const Register = () => {
                             <div>
                                 <label className="block" htmlFor="Name">Name</label>
                                 <input type="text" placeholder="Name"
-                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
                                     name="name"
                                     value={name}
                                     onChange={onChange}
@@ -92,7 +92,7 @@ const Register = () => {
                             <div className="mt-4">
                                 <label className="block" htmlFor="email">Email</label>
                                 <input type="email" placeholder="Email"
-                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
                                     name="email"
                                     value={email}
                                     onChange={onChange}
@@ -102,7 +102,7 @@ const Register = () => {
                             <div className="mt-4">
                                 <label className="block">Password</label>
                                 <input type="password" placeholder="Password"
-                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
                                     name="password"
                                     value={password}
                                     onChange={onChange}
@@ -112,7 +112,7 @@ const Register = () => {
                             <div className="mt-4">
                                 <label className="block">Confirm Password</label>
                                 <input type="password" placeholder="Password"
-                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+                                    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-600"
                                     name="password2"
                                     value={password2}
                                     onChange={onChange}
@@ -120,13 +120,13 @@ const Register = () => {
                                 />
                             </div>
                             <div className="flex">
-                                <button type='submit' className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 transition duration-200">
+                                <button type='submit' className="w-full px-6 py-2 mt-4 text-white bg-teal-500 rounded-lg hover:bg-teal-700 transition duration-200">
                                     Create Account
                                 </button>
                             </div>
                             <div className="mt-6 text-grey-dark">
                                 <span className="mr-2">Already have an account?</span>
-                                <Link to='/login' className="text-blue-600 hover:underline font-bold">
+                                <Link to='/login' className="text-teal-600 hover:underline font-bold">
                                     Log in
                                 </Link>
                             </div>
